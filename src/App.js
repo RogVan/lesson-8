38a2e958d8925300d0fe98e6f2d1269a3ba1f9e2
Roger V <roger.vanburgh@softwire.com>
2021-08-11 10:00:51 +0100

    Updated app to redirect to home if already logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 
@@ -11,7 +11,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions'
 
-function App({setCurrentUser}) {
+function App({currentUser, setCurrentUser}) {
 
 
   useEffect(()=>{
@@ -45,14 +45,18 @@ function App({setCurrentUser}) {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/signin' render={()=> currentUser ? <Redirect to='/'/> : <SignInAndSignUpPage/>}/>
       </Switch>
     </div>
   );
 }
 
+const mapStateToProps = ({user})=>({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
